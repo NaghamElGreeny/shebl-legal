0b1580aa1dffafdcc8379bd01a6a1fd352f9a766
naghamelgreeny <naghamelgreeny@gmail.com>
2025-05-05 17:15:00 +0300

    about us section

--- src/components/Sections/AboutUs.tsx
+++ src/components/Sections/AboutUs.tsx
@@ -4,14 +4,13 @@ import Link from "next/link";
 import PrimaryBtn from "../ui/PrimaryBtn"
 import { useTranslations } from 'next-intl';
 
-import ContactBtn from '../ui/ContactBtn';
 export default function name() {
     const t = useTranslations("About-section");
 
 
     return (
         <>
-            <div className="about-us bg-green-300 w-full h-[707px] grid grid-cols-2 gap-14">
+            <div className="about-us w-full h-[707px] grid lg:grid-cols-2 gap-14 ltr:ml-16">
 
                 <div className="group1 flex flex-col gap-14">
                     <div className="who-r-we flex flex-col gap-4 ">
@@ -22,26 +21,26 @@ export default function name() {
                             {t("description")}
                         </p>
                     </div>
-                    <div className="our-features grid grid-col-2  gap-4">
+                    <div className="our-features grid lg:grid-cols-2  gap-4">
                         <div className="feature-card flex">
-                            <img src={'/assets/icons/hero-title-icon.png'} /> <p>{t("card1")}</p></div>
+                            <img src={'/assets/icons/hero-title-icon.png'} /> <p style={{ color: 'var(--font-main)' }}>{t("card1")}</p></div>
                         <div className="feature-card flex">
-                            <img src={'/assets/icons/hero-title-icon.png'} /> <p>{t("card2")}</p></div>
+                            <img src={'/assets/icons/hero-title-icon.png'} /> <p style={{ color: 'var(--font-main)' }}>{t("card2")}</p></div>
                         <div className="feature-card flex">
-                            <img src={'/assets/icons/hero-title-icon.png'} /> <p>{t("card3")}</p></div>
+                            <img src={'/assets/icons/hero-title-icon.png'} /> <p style={{ color: 'var(--font-main)' }}>{t("card3")}</p></div>
                         <div className="feature-card flex">
-                            <img src={'/assets/icons/hero-title-icon.png'} /> <p>{t("card4")}</p></div>
+                            <img src={'/assets/icons/hero-title-icon.png'} /> <p style={{ color: 'var(--font-main)' }}>{t("card4")}</p></div>
                     </div>
-                    <div className="services-icons flex ">
+                    <div className="services-icons flex gap-4">
                         <img src={'assets/images/groupservices.png'} />
-                        <p>{t("subDescription")}</p>
+                        <p style={{ color: 'var(--font-sub2)' }}>{t("subDescription")}</p>
                     </div>
                     <div className="btn">
-                        <PrimaryBtn text={t("btnText")} arrow={t("arrow")} />
+                      <Link href={'/about-us'}>  <PrimaryBtn text={t("btnText")} arrow={t("arrow")} /></Link>
                     </div>
                 </div>
                 <div className="group2">
-                    <img src={'assets/images/group 1.png'} />
+                    <img src={'assets/images/group 1.png'} className='w-auto'/>
                 </div>
             </div>
         </>
