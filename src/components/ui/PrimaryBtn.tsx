9198db6755d03fca4c272e589f834bf659f331b3
nagham <naghamelgreeny@gmail.com>
2025-08-04 11:55:32 +0300

    btn

--- src/components/ui/PrimaryBtn.tsx
+++ src/components/ui/PrimaryBtn.tsx
@@ -11,8 +11,7 @@ export default function PrimaryBtn({ text, width, classs }: {
         <div className={`contact-primary-btn ${classs} flex flex-row justify-around content-center gap-1 ${width ? `w-[${width}]` : "w-[171px]"}`}>
             <div className='sm:text-sm xs:text-xs'>{text}</div>
             <div className="btn-icon">
-                {/* <Image src="/assets/icons/vector.png" alt="arrow-vector" width={24} height={24} className='ltr:rotate-180'/> */}
-                <img src="/assets/icons/vector.png" alt="arrow-vector" className='ltr:rotate-180'/>
+                <Image src="/assets/icons/Vector.png" alt="arrow-vector" width={24} height={24} className='ltr:rotate-180'/>
             </div>
         </div>
     )
